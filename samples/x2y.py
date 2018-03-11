1bdf008cc5077e0bf2af2182070c78f653d26e26
Vasiliy Poverennov <vasiliy.poverennov@dataart.com>
2018-03-11 20:42:37 +0300

    code style

--- samples/x2y.py
+++ samples/x2y.py
@@ -1,4 +1,4 @@
-from interception import ffi, lib as interception
+from interception import ffi, lib
 
 SCANCODE_X = 0x2D
 SCANCODE_Y = 0x15
@@ -8,25 +8,22 @@ if __name__ == '__main__':
 
     # raise_process_priority()
 
-    context = interception.interception_create_context()
+    context = lib.interception_create_context()
 
-    interception.interception_set_filter(
-        context, interception.interception_is_keyboard,
-        interception.INTERCEPTION_FILTER_KEY_DOWN | interception.INTERCEPTION_FILTER_KEY_UP,
-    )
+    lib.interception_set_filter(context, lib.interception_is_keyboard, lib.INTERCEPTION_FILTER_KEY_DOWN | lib.INTERCEPTION_FILTER_KEY_UP)
 
     stroke = ffi.new('InterceptionKeyStroke *')
     while True:
-        device = interception.interception_wait(context)
-        if not interception.interception_receive(context, device, stroke, 1):
+        device = lib.interception_wait(context)
+        if not lib.interception_receive(context, device, stroke, 1):
             break
 
         if stroke.code == SCANCODE_X:
             stroke.code = SCANCODE_Y
 
-        interception.interception_send(context, device, stroke, 1)
+        lib.interception_send(context, device, stroke, 1)
 
         if stroke.code == SCANCODE_ESC:
             break
 
-    interception.interception_destroy_context(context)
+    lib.interception_destroy_context(context)
