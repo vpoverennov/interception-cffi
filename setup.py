c1ef462d290c3505a7b69c5df04655f756edf37f
Vasiliy Poverennov <vasiliy@poverennov.com>
2025-02-03 17:08:14 -0300

    use released cffi version

--- setup.py
+++ setup.py
@@ -3,17 +3,17 @@ from setuptools import setup
 
 setup(
     name='interception',
-    version='0.6.dev0',
+    version='0.6.dev1',
     description='interception cffi bindings',
     author='Vasiliy Poverennov',
     author_email='vasiliy@poverennov.com',
     url='https://github.com/vpoverennov/interception-cffi',
-    setup_requires=['cffi @ git+https://github.com/python-cffi/cffi.git'],
+    setup_requires=['cffi>=1.17.0'],
     cffi_modules=[
         'src/interception_build.py:ffibuilder',
         'src/utils_build.py:ffibuilder',
     ],
-    install_requires=['cffi @ git+https://github.com/python-cffi/cffi.git'],
+    install_requires=['cffi>=1.17.0'],
     python_requires='>=3.8',
     packages=['interception', 'interception.samples'],
     package_dir={
